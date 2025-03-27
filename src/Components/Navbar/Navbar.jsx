f868b12e463ac789ed38587169cfea4f39ccf42f
Wizt <emireyesgutierres@gmail.com>
2025-03-26 23:11:03 -0600

    accesibility btn

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import './Navbar.css';
 import logo from '../../assets/logo.png';
 import menu_icon from '../../assets/menu-icon.png';
 import tache_icon from '../../assets/tache.png';
-import dropdown_icon from '../../assets/dropdown.png'; // Importa el icono de dropdown
-import closedown_icon from '../../assets/closedown.png'; // Importa el icono de cerrado
+import dropdown_icon from '../../assets/dropdown.png'; 
+import closedown_icon from '../../assets/closedown.png'; 
 import { Link } from "react-scroll";
 import { useLanguage } from "../../LanguageContext"; 
 
@@ -24,37 +24,37 @@ const Navbar = () => {
         };
     }, []);
 
+    useEffect(() => {
+        // Evitar añadir el script si ya existe
+        if (!document.querySelector('script[data-userway]')) {
+            const script = document.createElement("script");
+            script.src = "https://cdn.userway.org/widget.js";
+            script.dataset.account = "kjnkkEfZx0";
+            script.dataset.userway = "true"; // Atributo personalizado para identificarlo
+            script.async = true;
+            document.head.appendChild(script);
+        }
+    }, []);
+
     const toggleMenu = () => setMobileMenu(!mobileMenu);
     const toggleTrayectoriaMenu = () => setTrayectoriaMenu(!trayectoriaMenu);
 
     return (
         <div>
             {/* Botón flotante para WhatsApp */}
-            <a
-                className="botonFlotante"
-                href="https://wa.me/527779904960"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="¡Whasaaaa!"
-            >
+            <a className="botonFlotante" href="https://wa.me/527779904960" target="_blank" rel="noopener noreferrer" title="¡Whasaaaa!">
                 <i className="fab fa-whatsapp"></i>
             </a>
 
             {/* Botón flotante para subir */}
-            <a
-                className={`botonUp ${sticky ? 'dark-botonUp' : ''}`}
-                title="¡Pa' arriba!"
-            >
+            <a className={`botonUp ${sticky ? 'dark-botonUp' : ''}`} title="¡Pa' arriba!">
                 <Link to="presentacion" smooth={true} offset={0} duration={800}>
                     <i className='bx bx-chevron-up-circle'></i>
                 </Link>
             </a>
 
             {/* Botón flotante para bajar */}
-            <a
-                className={`botonDown ${sticky ? 'dark-botonDown' : ''}`}
-                title="¡Pa' abajo!"
-            >
+            <a className={`botonDown ${sticky ? 'dark-botonDown' : ''}`} title="¡Pa' abajo!">
                 <Link to="footer" smooth={true} offset={0} duration={1000}>
                     <i className='bx bx-chevron-down-circle'></i>
                 </Link>
@@ -78,20 +78,9 @@ const Navbar = () => {
                             {language === "es" ? "Planes" : "Plans"}
                         </Link>
                     </li>
-                    <li 
-                        className="dropdown" 
-                        onMouseEnter={toggleTrayectoriaMenu} 
-                        onMouseLeave={toggleTrayectoriaMenu}
-                    >
-                        <span>
-                            {language === "es" ? "Trayectoria" : "Trajectory"}
-                        </span>
-                        {/* Imagen dinámica para dropdown */}
-                        <img 
-                            src={trayectoriaMenu ? closedown_icon : dropdown_icon} 
-                            alt="Dropdown Icon" 
-                            className="dropdown-icon" 
-                        />
+                    <li className="dropdown" onMouseEnter={toggleTrayectoriaMenu} onMouseLeave={toggleTrayectoriaMenu}>
+                        <span>{language === "es" ? "Trayectoria" : "Trajectory"}</span>
+                        <img src={trayectoriaMenu ? closedown_icon : dropdown_icon} alt="Dropdown Icon" className="dropdown-icon" />
                         <ul className={`submenu ${trayectoriaMenu ? 'show' : ''}`}>
                             <li>
                                 <Link to="galeria" smooth={true} offset={-260} duration={500}>
@@ -111,17 +100,12 @@ const Navbar = () => {
                         </Link>
                     </li>
                 </ul>
+                
                 <div className="menu-container">
                     <div className="engBtn" onClick={toggleLanguage}>
                         {language === "es" ? "ENG" : "ESP"}
                     </div>
-                    {/* Cambio dinámico de la imagen del icono del menú */}
-                    <img 
-                        src={mobileMenu ? tache_icon : menu_icon} 
-                        alt="menu" 
-                        className="menu-icon" 
-                        onClick={toggleMenu} 
-                    />
+                    <img src={mobileMenu ? tache_icon : menu_icon} alt="menu" className="menu-icon" onClick={toggleMenu} />
                 </div>
             </nav>
         </div>
