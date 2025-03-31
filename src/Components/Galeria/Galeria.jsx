17f3e15374b59b02c279fb1d5525b0549d2bcc85
Wizt <emireyesgutierres@gmail.com>
2025-03-31 09:02:59 -0600

    imgs

--- src/Components/Galeria/Galeria.jsx
+++ src/Components/Galeria/Galeria.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import './Galeria.css';
 import galeria_1 from '../../assets/gallery-1.png';
+import galeria_1_Extend from '../../assets/gallery-1-extend.png';
 import galeria_2 from '../../assets/gallery-2.png';
+import galeria_2_Extend from '../../assets/gallery-2-extend.png';
 import galeria_3 from '../../assets/gallery-3.png';
+import galeria_3_Extend from '../../assets/gallery-3-extend.png';
 import galeria_4 from '../../assets/gallery-4.png';
-import flechaBlanca from '../../assets/dark-arrow.png';
+import galeria_4_Extend from '../../assets/gallery-4-extend.png';
+import flechaBlanca from '../../assets/dark-arrow.svg';
 import { useLanguage } from "../../LanguageContext";
+
+const imageMap = {
+    [galeria_1]: galeria_1_Extend,
+    [galeria_2]: galeria_2_Extend,
+    [galeria_3]: galeria_3_Extend,
+    [galeria_4]: galeria_4_Extend
+};
 
 const Galeria = () => {
     const { language } = useLanguage(); 
     const [selectedImage, setSelectedImage] = useState(null);
 
     const openModal = (image) => {
-        setSelectedImage(image);
+        setSelectedImage(imageMap[image]);
     };
 
     const closeModal = () => {
@@ -22,26 +33,14 @@ const Galeria = () => {
     return (
         <div className="galeria">
             <div className="gallery">
-                <img 
-                    src={galeria_1} 
-                    alt="Galería 1" 
-                    onClick={() => openModal(galeria_1)} 
-                />
-                <img 
-                    src={galeria_2} 
-                    alt="Galería 2" 
-                    onClick={() => openModal(galeria_2)} 
-                />
-                <img 
-                    src={galeria_3} 
-                    alt="Galería 3" 
-                    onClick={() => openModal(galeria_3)} 
-                />
-                <img 
-                    src={galeria_4} 
-                    alt="Galería 4" 
-                    onClick={() => openModal(galeria_4)} 
-                />
+                {[galeria_1, galeria_2, galeria_3, galeria_4].map((image, index) => (
+                    <img 
+                        key={index} 
+                        src={image} 
+                        alt={`Galería ${index + 1}`} 
+                        onClick={() => openModal(image)} 
+                    />
+                ))}
             </div>
             <a 
                 className="btn" 
