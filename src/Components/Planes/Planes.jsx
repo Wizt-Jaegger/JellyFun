388fc57f5d60479d734722f0d3e7d7233ed818d3
Wizt <emireyesgutierres@gmail.com>
2025-04-01 10:52:28 -0600

    imgs-png

--- src/Components/Planes/Planes.jsx
+++ src/Components/Planes/Planes.jsx
@@ -6,33 +6,41 @@ import plan_3 from '../../assets/program-3.png'
 import plan_icon_1 from '../../assets/program-icon-1.png'
 import plan_icon_2 from '../../assets/program-icon-2.png'
 import plan_icon_3 from '../../assets/program-icon-3.png'
+import { useLanguage } from "../../LanguageContext";
 
 const Planes = () => {
+    const { language } = useLanguage();
+    const plans = [
+        {
+            img: plan_1,
+            icon: plan_icon_1,
+            es: "Gelatinas Modernas",
+            en: "Modern Gelatine"
+        },
+        {
+            img: plan_2,
+            icon: plan_icon_2,
+            es: "Gelatinas Caseras",
+            en: "Homemade Gelatine"
+        },
+        {
+            img: plan_3,
+            icon: plan_icon_3,
+            es: "Gelatinas sorpresa",
+            en: "Surprise Gelatine"
+        }
+    ];
     return(
         <div className='planes'>
-            <div className="plan">
-                <img src={plan_1} alt=''/>
-                <div className='subtitulo'>
-                    <img src={plan_icon_1} alt=''/>
-                    <p>Gelatinas Modernas</p>
+            {plans.map((plan, index) => (
+                <div className="plan" key={index}>
+                    <img src={plan.img} alt='' />
+                    <div className='subtitulo'>
+                        <img src={plan.icon} alt='' />
+                        <p>{language === "es" ? plan.es : plan.en}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="plan">
-                <img src={plan_2} alt=''/>
-                <div className='subtitulo'>
-                    <img src={plan_icon_2} alt=''/>
-                    <p>Gelatinas Caseras</p>
-                </div>
-            </div>
-            <div className="plan">
-                <img src={plan_3} alt=''/>
-                <div className='subtitulo'>
-                    <img src={plan_icon_3} alt=''/>
-                    <p>Gelatinas sorpresa</p>
-                </div>
-            </div>
-            
-
+            ))}
         </div>
     )
 }
